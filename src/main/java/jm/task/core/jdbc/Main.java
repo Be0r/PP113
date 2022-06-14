e3845d4f58a68ec2f3dbdd824626b4179f6a34f0
Be0r <103316591+Be0r@users.noreply.github.com>
2022-06-14 17:22:21 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,37 @@
-package jm.task.core.jdbc;
+﻿package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        User user1 = new User("Vasya", "Pupkin", (byte) 8);
+        User user2 = new User("Vano", "Ivanko", (byte) 6);
+        User user3 = new User("Petr", "Petrov", (byte) 12);
+        User user4 = new User("Foma", "Fomin", (byte) 10);
+
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        System.out.println("User named " + user1.getName() + " has been added to the database");
+
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println("User named " + user2.getName() + " has been added to the database");
+
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println("User named " + user3.getName() + " has been added to the database");
+
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println("User named " + user4.getName() + " has been added to the database");
+
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user.toString());
+        }
+
+        //
+        userService.cleanUsersTable();
+        //userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Glazok83");
+
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database");
+        }
+        return connection;
+    }
 }
